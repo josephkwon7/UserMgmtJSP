727f1e3d3fb4ccaa5af8f3c82a6bc849d67dc3b9
josephkwon <devel.kwon@gmail.com>
2018-09-12 14:11:34 +0900

    implement returning of connection related resouces (video 25)

--- src/net/slipp/user/UserDAO.java
+++ src/net/slipp/user/UserDAO.java
@@ -23,48 +23,101 @@ public class UserDAO {
     
     public void addUser(User user) throws SQLException {
         String sql = "INSERT INTO users VALUES(?, ?, ?, ?)";
-        PreparedStatement pstmt = getConnection().prepareStatement(sql);
-        pstmt.setString(1, user.getUserId());
-        pstmt.setString(2, user.getPassword());
-        pstmt.setString(3, user.getName());
-        pstmt.setString(4, user.getEmail());
-        System.out.println(pstmt.toString());
-        
-        pstmt.executeUpdate();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, user.getUserId());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getName());
+            pstmt.setString(4, user.getEmail());
+            System.out.println(pstmt.toString());
+            
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
     }
 
     public User findByUserId(String userId) throws Exception {
         String sql = "SELECT * FROM users WHERE userId = ?";
-        PreparedStatement pstmt = getConnection().prepareStatement(sql);
-        pstmt.setString(1, userId);
         
-        ResultSet rs = pstmt.executeQuery();
-        
-        if (!rs.next()) {
-            return null;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, userId);
+            
+            rs = pstmt.executeQuery();
+            
+            if (!rs.next()) {
+                return null;
+            }
+            return new User(
+                    rs.getString("userId"),
+                    rs.getString("password"),
+                    rs.getString("name"),
+                    rs.getString("email")
+                    );
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
         }
-        return new User(
-                rs.getString("userId"),
-                rs.getString("password"),
-                rs.getString("name"),
-                rs.getString("email")
-                );
     }
 
     public void removeUser(String userId) throws SQLException {
         String sql = "DELETE FROM users WHERE userId = ?";
-        PreparedStatement pstmt = getConnection().prepareStatement(sql);
-        pstmt.setString(1, userId);
-        pstmt.executeUpdate();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, userId);
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
     }
 
     public void updateUser(User user) throws SQLException {
         String sql = "UPDATE users SET password=?, name=?, email=? WHERE userId=?";
-        PreparedStatement pstmt = getConnection().prepareStatement(sql);
-        pstmt.setString(1, user.getPassword());
-        pstmt.setString(2, user.getName());
-        pstmt.setString(3, user.getEmail());
-        pstmt.setString(4, user.getUserId());
-        pstmt.executeUpdate();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
     }
 }
